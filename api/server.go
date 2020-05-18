dce6b2f037541cc9a303a0170dc3aa6517cf154b
DixonOrtiz <dixon.ortizc@gmail.com>
2020-05-18 11:56:27 -0400

    user-api routes and controllers finished

--- api/server.go
+++ api/server.go
@@ -30,7 +30,6 @@ func Run() {
 	router.HandleFunc("/user/devices", controllers.GetDevices).Methods("GET")         //add JWT middleware
 	router.HandleFunc("/user/saveDevice", controllers.SaveDevice).Methods("POST")     //add JWT middleware
 	router.HandleFunc("/user/changeDevice", controllers.ChangeDevice).Methods("POST")
-	// router.HandleFunc("/user/linkUser/{userID}/{deviceID}", controllers.LinkUserToDevice).Methods("PUT") //add admin middleware
 	router.HandleFunc("/user/allDevices", controllers.GetAllDevices).Methods("GET")               //add admin middleware
 	router.HandleFunc("/user/device/{deviceID}/user", controllers.GetUserByDevice).Methods("GET") //add admin middleware
 
